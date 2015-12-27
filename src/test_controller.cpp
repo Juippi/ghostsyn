33c6f1d7483602b53d2c9557040bd4a840b03c72
Juho Heikkinen <juho.heikkinen@iki.fi>
2015-12-27 19:17:08 +0200

    Add testing for Controller.

---
+++ src/test_controller.cpp
@@ -0,0 +1,9 @@
+#define BOOST_TEST_MODULE test controller
+#define BOOST_TEST_DYN_LINK
+#include <boost/test/unit_test.hpp>
+
+#include "controller.hpp"
+
+BOOST_AUTO_TEST_CASE(controller_create) {
+    Controller ctrl;
+}
