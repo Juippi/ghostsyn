93934a4fd8e38dd3626c7dc3520800d324e366a7
Juho Heikkinen <juho.heikkinen@iki.fi>
2018-10-16 01:51:33 +0300

    Implement multiple delay taps correctly in reverb

--- src/synth/module_reverb.asm
+++ src/synth/module_reverb.asm
@@ -12,8 +12,8 @@
 %define REV_STATE_FLT		4
 %define REV_STATE_BUF		8	; delay buffer start offset
 
-%define REV_MAGIC 7391		; magic constant for generating delay tap locations
-%define DELAY_LEN_MASK 0x1ffff	; for 32768 sample delay buffer (4 bytes / sample)
+%define REV_MAGIC 19391		; magic constant for generating delay tap locations
+%define DELAY_LEN_MASK 0x1fffc	; for 32768 sample delay buffer (4 bytes / sample)
 
 module_reverb:
 	pusha
@@ -26,26 +26,30 @@ module_reverb:
 	and ebx, DELAY_LEN_MASK
 	mov [ebp + REV_STATE_DELAY_POS], ebx
 
-	push ebx		; store pos. of next delay input value
-
 	;; sample delayline at multiple positions
 
 	mov ecx, [esi + REV_VAL_TAPS] ; number of delay taps
 	fldz			      ; delay line output accumulator
 
-	.delay_tap_loop
-	fadd dword [ebp + ebx + REV_STATE_BUF] ; sample delay line
-	imul ebx, REV_MAGIC
-	add ebx, REV_MAGIC
-	and ebx, DELAY_LEN_MASK
-	loop .delay_tap_loop
+	xor eax, eax		; keeps delay line sample offset during loop
+delay_tap_loop:
+	;; calculate offset of next tab ("pseudorandom")
+	imul eax, REV_MAGIC
+	add eax, REV_MAGIC
 
-	;; TODO: lp filter for delay line output
+	push ebx		; store original ringbuffer pos
+	add ebx, eax
+	and ebx, DELAY_LEN_MASK
+	fadd dword [ebp + ebx + REV_STATE_BUF] ; sample delay line
+	pop ebx
+
+	loop delay_tap_loop
+
+	;; TODO: lp filter for delay line input
 
 	fmul dword [esi + REV_VAL_FEEDBACK]
 	faddp
 
-	pop ebx				      ; restore pos. of next delay input value
 	fst dword [ebp + ebx + REV_STATE_BUF] ; store output to delayline
 	
 	popa
